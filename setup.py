cd18f64a522b744e6437eb4f40cd9dd9e47f014d
wholden <holdenwm523@gmail.com>
2018-01-15 21:34:17 -0500

    initial commit, tools for rixs data from Minisoft

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "RixsProcessing",
+    version = "0.1",
+    author = "William",
+    description = ("Basic tools for analyzing RIXS data. Aimed at data taken on the Minisoft spectrometer platform."),
+    packages=['RixsProcessing']
+)
